867048bc5809acf7419abd45ab513e50f3859531
Emil Asplund <emilasplund86@gmail.com>
2018-07-11 07:30:08 +0200

    Adding blogpost schema

--- models/blogPost.js
+++ models/blogPost.js
@@ -1,2 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+/*
+ * ========================================= 
+ * Create a blog post schema and model
+ * ========================================= 
+ */
+
+ const blogPostSchema = new Schema({
+     title: {
+         type: String,
+         required: [true, 'Title is required']
+     },
+     content: {
+         type: String,
+         required: [true, 'Content is required']
+     },
+    createdAt: {
+         type: Date,
+         default: Date.now
+     }
+ });
